95403dc05e239fa4c3733d3f8a53d6269bb3abd5
SPARROW69 <140163440+landryk69@users.noreply.github.com>
2023-11-24 12:16:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let boutons = document.querySelectorAll(".bouton");
+let input = document.querySelector("input")
+let arr = Array.from(boutons);
+let str = ""
+arr.forEach(btn =>{btn.addEventListener("click",(e)=>{
+	if (e.target.innerText == '='){
+		
+		str = eval(str)
+		input.value = str;
+		console.log(str)
+	}
+
+	else if(e.target.innerText == 'C'){
+		str = "";
+		input.value = str
+	}
+
+	else if(e.target.innerText == '⟵'){
+		console.log(e.target.innerText)
+		str =  str.substring(0, str.length-1);
+		input.value = str;
+	}
+
+	else if(e.target.innerText == "+") {
+		if (str.includes("+")) {
+			str = str
+		}else{
+			str = str + e.target.innerText;
+		}
+	}
+
+	else if(e.target.innerText == "-") {
+		if (str.includes("-")) {
+			str = str
+		}else{
+			str = str + e.target.innerText;
+		}
+	}
+
+	else if(e.target.innerText == "/") {
+		if (str.includes("/")) {
+			str = str
+		}else{
+			str = str + e.target.innerText;
+		}
+	}
+
+	else if(e.target.innerText == "*") {
+		if (str.includes("*")) {
+			str = str
+		}else{
+			str = str + e.target.innerText;
+		}
+	}
+
+	else{
+		str = str + e.target.innerText;
+		input.value = str;
+		console.log(str)
+	}
+
+})});
+
+
+
